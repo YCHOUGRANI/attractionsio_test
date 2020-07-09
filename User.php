481d111408317d9b8660ff8f03d924162a6a570b
Youcef CHOUGRANI <ychougrani@hotmail.com>
2020-07-09 07:43:19 +0100

    update class User

---
+++ User.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace AttractionsIo\Domain;
+
+class User
+{
+
+    /**
+     * @var string
+     */
+    private $first_name;
+
+    /**
+     * @var string
+     */
+    private $last_name;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @var mix
+     */
+    private $date_of_birth;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    public function __construct(
+        string $first_name,
+        string $last_name,
+        string $date_of_birth,
+        string $email,
+        string $password
+    ) {
+
+        if (strlen($first_name) > 32) {
+            throw new \InvalidArgumentException("first_name only max 32 chars allowed");
+        }
+        if (strlen($last_name) > 32) {
+            throw new \InvalidArgumentException("last_name only max 32 chars allowed");
+        }
+
+        if (strlen($email) > 254) {
+            throw new \InvalidArgumentException("email only max 254 chars allowed");
+        }
+        if (strlen($password) > 32) {
+            throw new \InvalidArgumentException("email only max 32 chars allowed");
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException("email is not valid format");
+        }
+        $this->first_name = $first_name;
+        $this->first_name = $first_name;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name)
+    {
+        if (strlen($first_name) > 32) {
+            throw new \InvalidArgumentException("first_name only max 32 chars allowed");
+        }
+        $this->first_name = $first_name;
+    }
+
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name)
+    {
+        if (strlen($last_name) > 32) {
+            throw new \InvalidArgumentException("last_name only max 32 chars allowed");
+        }
+        $this->last_name = $last_name;
+    }
+
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email)
+    {
+        if (strlen($email) > 254) {
+            throw new \InvalidArgumentException("email only max 254 chars allowed");
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException("email is not valid format");
+        }
+
+        $this->email = $email;
+    }
+
+
+    public function getDateOfBirth()
+    {
+        return $this->date_of_birth;
+    }
+
+    public function setDateOfBirth(string $date_of_birth)
+    {
+
+        $minAge = strtotime("-13 YEAR");
+        $entrantAge = strtotime($date_of_birth);
+
+        if ($entrantAge < $minAge) {
+            throw new \InvalidArgumentException("you are too young to register");
+        }
+    }
+}
--- test/UserTest.php
+++ test/UserTest.php
@@ -1,33 +1,51 @@
 <?php
 require '..\app\user.php';
+
 use PHPUnit\Framework\TestCase;
 
 
 class  UserTest extends TestCase
 {
-		 protected $user;
-		 
-		 public function setUp()
-		 {
-		 $this->user=new \AttractionsIo\Domain\User();
-         }
-        
-        
-         public function testCannotBeCreatedFromInvalidEmailAddress(): void
-         {
+    protected $user;
+
+    public function setUp()
+    {
+        $this->user = new \AttractionsIo\Domain\User();
+    }
+
+
+    public function testEmailCannotBeCreatedFromInvalidEmailAddress(): void
+    {
         $this->expectException(InvalidArgumentException::class);
 
         $result = $this->user->setEmail('invalid');
-         }
+    }
 
-    public function testCanBeUsedAsString(): void
+    public function testEmailCanBeUsedAsString(): void
     {
 
         $this->user->setEmail('Jack@attractions.internal');
         $this->assertEquals(
             'Jack@attractions.internal',
-            $uset->getEmail()
+            $user->getEmail()
         );
     }
 
+    public function testDateOfBirthCanBeUsedAsString(): void
+    {
+
+        $this->user->setetDateOfBirth('15/05/1991');
+        $this->assertEquals(
+            '15/05/1991',
+            $user->getDateOfBirth()
+        );
+    }
+
+    public function testDateOfBirthCCannotBeCreatedFromAgeLessThan13(): void
+    {
+
+        $this->expectException(InvalidArgumentException::class);
+
+        $result = $this->user->setEmail('02/01/2010');
+    }
 }
